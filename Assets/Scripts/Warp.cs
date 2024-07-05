88dfa4207aa4c2ccdc444d4e52b3c11c17bdd4a1
DStol123 <dennis.stolbovoy@hotmail.com>
2024-07-05 16:14:02 +0900

    ワープ後の速度限界を作りました

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -6,11 +6,13 @@ public class Warp : MonoBehaviour
 {
     public GameObject WarpEnd;
     float warpOffset;
+    [SerializeField]
+    private float MaxSpeed;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         other.transform.position = WarpEnd.transform.position + warpOffset * WarpEnd.transform.forward;
         other.transform.rotation = WarpEnd.transform.rotation;
-        other.attachedRigidbody.velocity = other.attachedRigidbody.velocity.magnitude * WarpEnd.transform.forward;
+        other.attachedRigidbody.velocity = Vector3.ClampMagnitude(other.attachedRigidbody.velocity.magnitude * WarpEnd.transform.forward, MaxSpeed);
     }
 }
